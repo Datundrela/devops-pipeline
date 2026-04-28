8b04a87fdc172eca4927e8356b0950ace68b438b
Datundrela <sadaghashvili.davit@kiu.edu.ge>
2026-04-28 16:38:20 +0400

    change status code to cause error in test

--- app.test.js
+++ app.test.js
@@ -8,6 +8,6 @@ afterAll(() => {
 describe('GET /', () => {
   it('should return 200 OK', async () => {
     const res = await request(app).get('/');
-    expect(res.statusCode).toEqual(200);
+    expect(res.statusCode).toEqual(404);
   });
 });
